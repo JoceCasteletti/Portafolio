1037e83bae88fda99b89e5271f1178f97beb733d
Jocelyn Casteletti <jocelyncasteletti@gmail.com>
2019-09-04 03:47:54 -0400

    se agrega links en botón Demo de projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -13,41 +13,41 @@ export class ProjectsComponent implements OnInit {
     {
       image: 'src="../../assets/img/mdlinks.png',
       title: 'Markdown Links',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.',
+      description: 'Librería para extraer los enlaces de directorios y/o archivos markdown (.md), validar su status. Fue desarrollada usando Node.js.',
       urlDemo: 'https://www.npmjs.com/package/@jocecasteletti/md-links',
-      urlRepo: 'https://nic.cl'
+      urlRepo: 'https://github.com/JoceCasteletti/SCL009-md-links'
     },{
       image: 'src="../../assets/img/otrasmiradas.png',
       title: 'Fundación Otras Miradas',
-      description: 'Aplicación para leer links en archivos Markdown.',
+      description: 'Página web diseñada para usuarios reales de la fundación Otras Miradas. Fué desarrollada usando Angular , Firebase y Bootrstap.',
       urlDemo: 'https://otrasmiradas01.firebaseapp.com/home',
-      urlRepo: 'https://nic.cl'
+      urlRepo: 'https://github.com/JoceCasteletti/SCL009-Otras-Miradas'
     },
     {
       image: 'src="../../assets/img/maps.png',
       title: 'Proyecto Maps',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.',
+      description: 'Aplicación que permite visualizar mapa de Google Maps con tiendas de México. Además, es posible agregar stores a favoritos y eliminarlos. Fué desarrollado usando Javascript, Angular y consumo de la api de Google Maps.',
       urlDemo: 'https://favorites-maps.firebaseapp.com/',
-      urlRepo: 'https://nic.cl'
+      urlRepo: 'https://github.com/JoceCasteletti/SCL009-Desafio-Maps'
     },
     {
       image: 'src="../../assets/img/burgerqueen.png',
       title: 'Proyecto Burger Queen',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.',
+      description: 'Aplicación desarrollada para ser usada en Tablet, permite a los trabajadores de esta cadena de comida almacenar los pedidos de los clientes. Fué desarrollado usando Angular, Bootstrap y Firebase para almacenar los pedidos realizados.',
       urlDemo: 'https://scl009-burger-queen.firebaseapp.com/',
-      urlRepo: 'https://nic.cl'
+      urlRepo: 'https://github.com/JoceCasteletti/SCL009-Burger-Queen/tree/develop'
     }, {
       image: 'src="../../assets/img/infopokemon.png',
       title: 'Info Pokemon',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.',
+      description: 'Trabajo realizado con información de data Pokémon de la región de Kanto, permitiendo al usuario realizar diversos filtros. Fué desarrollada usando Javascript y también se utilizó Bootstrap.',
       urlDemo: 'https://jocecasteletti.github.io/SCL009-data-lovers/src/index.html',
-      urlRepo: 'https://nic.cl'
+      urlRepo: 'https://github.com/JoceCasteletti/SCL009-data-lovers'
     }, {
       image: 'src="../../assets/img/redsocial.png',
       title: 'Red social Viajeros Chilenos',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.',
+      description: 'Red social que permite que los viajeros puedan encontrar un dato de utilidad para sus viajes. Fué desarrolla usando Javascript, Bootstrap y Firebase para realizar la autentificación de usuarios.',
       urlDemo: 'https://majomarquez.github.io/SCL009-Social-Network/src/index.html#/home',
-      urlRepo: 'https://nic.cl'
+      urlRepo: 'https://github.com/JoceCasteletti/SCL009-Social-Network'
     },
   ];
 
